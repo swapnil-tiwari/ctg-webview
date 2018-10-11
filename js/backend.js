f490ee1b2f56094bba64f3cbce92836b5251807a
Swapnil Tiwari <36299079+swapnil-tiwari@users.noreply.github.com>
2018-10-11 22:29:26 +0530

    UI Update+ Complete Reg Update+Bug Fixes

--- js/backend.js
+++ js/backend.js
@@ -104,13 +104,18 @@ document.write(JSON.stringify(res))
             //     },
             //     body:JSON.stringify({username:username_f.value,password:pass_f.value,email:email_f.value,contact:contactno_f.value})
             // })
-            var response=await api.register({username:username_f.value,password:pass_f.value,email:email_f.value,contact:contactno_f.value});
-            if(response.code==200)
-            {
-               window.location.href = "../pages/login.html#"+hash;
-            }
-            else {
-               window.location.href = "../pages/getting-started.html";
+            try {
+                  var response=await api.register({username:username_f.value,password:pass_f.value,email:email_f.value,contact:contactno_f.value});
+                  if(response.code==200)
+                  {
+                     window.location.href = "../pages/login.html#"+hash;
+                  }
+
+
+
+            } catch (e) {
+                window.alert("Invalid Details Entered");
+
 
             }
             //resp.value=JSON.stringify(await res.json())
@@ -130,17 +135,39 @@ document.write(JSON.stringify(res))
             //     },
             //     body:JSON.stringify({username:username_sp.value,password:pass_sp.value,email:email_sp.value,contact:contactno_sp.value})
             // })
-            var response=await api.register({username:username_sp.value,password:pass_sp.value,email:email_sp.value,contact:contactno_sp.value});
-            if(response.code==200)
-            {
-               window.location.href = "../pages/login.html#"+hash;
-            }
-            else {
-               window.location.href = "../pages/getting-started.html";
+            try {
+                var response=await api.register({username:username_sp.value,password:pass_sp.value,email:email_sp.value,contact:contactno_sp.value});
+                if(response.code==200)
+                {
+                   window.location.href = "../pages/login.html#"+hash;
+                }
 
+            } catch (e) {
+              window.alert("Invalid Details Entered");
             }
+
             //resp.value=JSON.stringify(await res.json())
             //console.log(JSON.stringify(await res.json()));
+
+
+        }
+        async function register_ind()
+        {
+            var hash=window.location.hash="individual";
+            window.api?null:window.api=await setAPI();
+            try {
+                  var response=await api.register({username:username_ind.value,password:pass_ind.value,email:email_ind.value,contact:contactno_ind.value});
+                  if(response.code==200)
+                  {
+                     window.location.href = "../pages/login.html#"+hash;
+                  }
+
+
+
+            } catch (e) {
+                window.alert("Invalid Details Entered");
+            }
+
 
 
         }
@@ -152,17 +179,7 @@ document.write(JSON.stringify(res))
         {
           document.location.reload();
         }
-        else {
-          console.log("Some Error");
-        }
-        // var res=await fetch('http://localhost:8080/data/logout',
-        // {
-        //     method:'get',
-        //     headers:{
-        //         'content-type':'application/json'
-        //     }
-        // })
-        //resp.value=JSON.stringify(await res.json())
+
 
     }
     // async function islogin()
@@ -184,7 +201,7 @@ document.write(JSON.stringify(res))
       window.api?null:window.api=await setAPI();
       try
       {
-        var response=await api["register/update"]({communicationlanguage:comlanguage.value,skillslevel:skill,description:description.value,work:work.value,registrationstatus:"completed"});
+        var response=await api["register/update"]({communicationlanguage:comlanguage.value,skillslevel:skill,description:description.value,github:github.value,linkedin:linkedin.value,work:work.value,registrationstatus:"completed"});
         if (response.code==200)
         {
            window.location.href =window.location.origin
