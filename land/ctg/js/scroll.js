5d3017cee64479cc49aa3dbfecc90829f68e49ba
Swapnil Tiwari <36299079+swapnil-tiwari@users.noreply.github.com>
2018-10-14 12:09:11 +0530

    Bug Fixes

--- land/ctg/js/scroll.js
+++ land/ctg/js/scroll.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   $(window).scroll(function(){
   	var scroll = $(window).scrollTop();
 	  if (scroll > 100) {
-	    $(".bg-new").css("background" , "#fff");
+	    $(".bg-new").css({"background":"#fff","box-shadow":"0 4px 5px 0 rgba(0,0,0,0.14), 0 1px 10px 0 rgba(0,0,0,0.12), 0 2px 4px -1px rgba(0,0,0,0.2);"});
 	  }
 
 	  else{
